2db0e87262d8f1f5da0e33e273622a1dcf88eaa0
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-28 21:59:27 +0800

    Created program 5 that corrects the capitalization for the input

---
+++ correct_capitalization_for_inputted_name.py
@@ -0,0 +1,8 @@
+#Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing
+
+#Input user's name
+input_name = input("Enter your name: ")
+
+#Print in lower case
+print("Your name in proper casing is:",input_name.title())
+
