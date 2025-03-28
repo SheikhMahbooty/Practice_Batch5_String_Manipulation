baf4073e3f489259e2d12648671e2e1092f2473f
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-28 22:52:18 +0800

    Created program 8 that counts the characters in words

---
+++ word_character_counter.py
@@ -0,0 +1,10 @@
+#Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+#Input text
+input_name = input("Enter your name: ")
+
+#Counts the words by the characters including spaces
+character_count = len(input_name)
+
+#Print the number of characters
+print("Number of characters:", character_count)
