030b241472f9714d9a1967aa95002867836afdc1
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-28 21:38:27 +0800

    Created and uploaded Program 1 removing leading spaces

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -1,10 +1,10 @@
-# Prog01: Remove leading spaces from fullname
+# Prog01: Remove leading spaces from inputted name
 
 # Ask the user to input their full name with spaces at the beginning
 input_name = input("Input: ")
 
 # Remove spaces 
 stripped_name = input_name.lstrip()
- 
+
 # Print the cleaned name
 print("output:", stripped_name)
