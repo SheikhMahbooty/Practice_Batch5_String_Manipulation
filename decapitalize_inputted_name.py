cfe80e1a6de000a501fe4b59a2b01e64a5aac917
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-28 21:55:45 +0800

    Created program 4 which decapitalizes the inputted name

---
+++ decapitalize_inputted_name.py
@@ -0,0 +1,7 @@
+#Prog04: Create a program that ask the user to input their fullname. Print the input in lower case.
+
+#Input user's name
+input_name = input("Enter your name: ")
+
+#Print in lower case
+print("Your name in caps is:",input_name.lower())
