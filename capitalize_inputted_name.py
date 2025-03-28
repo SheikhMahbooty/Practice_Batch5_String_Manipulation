4ac1fc7cbfabc0cdb77c0b34b9b9b8ea56595efb
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-28 21:52:19 +0800

    Created program 3 that capitalizes an inputted word/name

---
+++ capitalize_inputted_name.py
@@ -0,0 +1,7 @@
+#Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+#Input user's name
+input_name = input("Enter your name: ")
+
+#Print in capital letters
+print("Your name in caps is:",input_name.upper())
