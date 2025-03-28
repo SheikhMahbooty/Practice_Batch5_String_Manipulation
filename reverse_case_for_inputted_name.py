a873ee38b80eda02f459be82b36815ccbf10006b
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-03-28 22:36:50 +0800

    Created program 6 that reverses the case of letters

---
+++ reverse_case_for_inputted_name.py
@@ -0,0 +1,7 @@
+#Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+#Input user's name
+input_name = input("Enter your name: ")
+
+#Print in improper casing
+print("Your name in improper casing is:",input_name.swapcase())
